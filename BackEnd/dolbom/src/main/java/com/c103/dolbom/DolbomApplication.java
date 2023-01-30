c7294c6a4adf08636da2752f0e2d84cf7dcded7d
이충무 <lcm2822@daum.net>
2023-01-30 11:30:37 +0900

    initial commit

---
+++ BackEnd/dolbom/src/main/java/com/c103/dolbom/DolbomApplication.java
@@ -0,0 +1,13 @@
+package com.c103.dolbom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DolbomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DolbomApplication.class, args);
+	}
+
+}
---
+++ BackEnd/dolbom/src/test/java/com/c103/dolbom/DolbomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.c103.dolbom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DolbomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
