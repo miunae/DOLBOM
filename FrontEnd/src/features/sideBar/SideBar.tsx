ba6e5f01ff582acaca60fe42ad0d243c66d24340
binslog <bins95@naver.com>
2023-02-02 17:56:00 +0900

    0202 openvidu

--- FrontEnd/src/features/sideBar/SideBar.tsx
+++ FrontEnd/src/features/sideBar/SideBar.tsx
@@ -6,6 +6,7 @@ import PersonSearchIcon from '@mui/icons-material/PersonSearch';
 import VideoCameraFrontIcon from '@mui/icons-material/VideoCameraFront';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import Divider from '@mui/material/Divider';
 import Drawer from '@mui/material/Drawer';
@@ -14,9 +15,11 @@ import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
+import Modal from '@mui/material/Modal';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import * as React from 'react';
+import axios from 'axios';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import { useAppSelector } from '../../app/hooks';
@@ -25,16 +28,40 @@ import { selectUser } from '../auth/userSlice';
 import { clearUser } from '../auth/userSlice';
 
 const drawerWidth = 240;
+const style = {
+  position: 'absolute' as 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
 
 export const SideBar = () => {
   const dispatch = useAppDispatch();
   const user = useAppSelector(selectUser);
+
+  // const [modal, setModal] = useState(false);
+  // console.log(modal);
 
   const logOut = () => {
     dispatch(clearUser());
     sessionStorage.removeItem('access-token');
     sessionStorage.removeItem('refresh-token');
   };
+
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const getClient = async () => {
+    const client = await axios.get('https://localhost:3000/api/client/{id}');
+    console.log(client.data);
+  };
+
   return (
     <Box sx={{ display: 'flex' }}>
       <CssBaseline />
@@ -82,16 +109,64 @@ export const SideBar = () => {
             </ListItem>
           </Link>
         </List>
+
         <List>
-          <Link to="/video" style={{ textDecoration: 'none', color: 'inherit' }}>
+          <div style={{ textDecoration: 'none', color: 'inherit' }}>
             <ListItem key="video" disablePadding>
               <ListItemButton>
                 <VideoCameraFrontIcon />
-                <ListItemText primary="Video" />
+                <div>
+                  <Button
+                    onClick={() => {
+                      getClient();
+                      handleOpen();
+                    }}
+                  >
+                    Video
+                  </Button>
+                  <Modal
+                    open={open}
+                    onClose={handleClose}
+                    aria-labelledby="modal-modal-title"
+                    aria-describedby="modal-modal-description"
+                  >
+                    <Box sx={style}>
+                      <Typography id="modal-modal-title" variant="h6" component="h2">
+                        내담자 정보 입력 및 세션 생성
+                      </Typography>
+                      <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                        내담자 선택 :
+                        <select name="client">
+                          <option value="park">김다미</option>
+                          <option value="cool">김태리</option>
+                          <option value="cool">금태록</option>
+                        </select>
+                      </Typography>
+                      <div>
+                        <Typography id="modal-modal-session" sx={{ mt: 2 }}>
+                          세션 번호
+                        </Typography>
+                        <input defaultValue={'정수 값'} />
+                      </div>
+
+                      <Typography id="modal-modal-button" variant="h6" component="h2">
+                        <Link to="/video">
+                          <button>세션 생성</button>
+                        </Link>
+                      </Typography>
+                      <Typography id="modal-modal-button" variant="h6" component="h2">
+                        <Link to="/calendar ">
+                          <button onClick={handleClose}>뒤로가기</button>
+                        </Link>
+                      </Typography>
+                    </Box>
+                  </Modal>
+                </div>
               </ListItemButton>
             </ListItem>
-          </Link>
+          </div>
         </List>
+
         <List>
           <Link to="/userprofile" style={{ textDecoration: 'none', color: 'inherit' }}>
             <ListItem key="userprofile" disablePadding>
@@ -103,6 +178,7 @@ export const SideBar = () => {
           </Link>
         </List>
         <Divider />
+
         {/* 로그아웃 버튼 & 기능 */}
         <List>
           <ListItem key="Logout" disablePadding>
--- FrontEnd/src/pages/VideoPage.tsx
+++ FrontEnd/src/pages/VideoPage.tsx
@@ -1,16 +1,38 @@
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
 import Grid from '@mui/material/Grid';
+import Modal from '@mui/material/Modal';
+import Typography from '@mui/material/Typography';
 import * as React from 'react';
 
+import VideoRoomComponent from '.././components/VideoRoomComponent';
 import { SideBar } from '../features/sideBar/SideBar';
+
+const style = {
+  position: 'absolute' as 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+
 export const VideoPage = () => {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
   return (
     <>
       <Grid container spacing={2}>
         <Grid item xs={2}>
-          <SideBar />
+          {/* <SideBar /> */}
         </Grid>
         <Grid item xs={10}>
-          <h1>Viedo Conferencing</h1>
+          <h1>Video Conferencing</h1>
+          <VideoRoomComponent />
         </Grid>
       </Grid>
     </>
