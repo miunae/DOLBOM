c89edf4afc130f2e731805fe6501ddf57857c10a
Jongsu Park <pjs12041204@naver.com>
2023-02-02 17:44:45 +0900

    STYLE: schedule 엔티티 생성

--- BackEnd/dolbom/src/main/java/com/c103/dolbom/Entity/Schedule.java
+++ BackEnd/dolbom/src/main/java/com/c103/dolbom/Entity/Schedule.java
@@ -1,4 +1,32 @@
 package com.c103.dolbom.Entity;
 
-public class Schedule {
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import java.time.LocalDateTime;
+
+@Getter
+@Entity(name="schedule")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Schedule extends BaseTimeEntity{
+
+    @ManyToOne
+    @JoinColumn(name = "id")
+    private MemberClient memberClient;
+
+    @Column(name="start_time")
+    private LocalDateTime startTime;
+    @Column(name="end_time")
+    LocalDateTime endTime;
+    @Column(columnDefinition = "text")
+    private String content;
+
 }
