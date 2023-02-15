58dfcdd4bd79808aad2cd80fc8f6f52fea25389c
박현우 <a40293865@gmail.com>
2023-02-15 20:38:43 +0900

    shelter

---
+++ FrontEnd/src/features/clientManagement/Loading.tsx
@@ -0,0 +1,10 @@
+import LinearProgress from '@mui/material/LinearProgress';
+import Typography from '@mui/material/Typography';
+export const Loading = () => {
+  return (
+    <>
+      <Typography variant="h3">Responsive h3</Typography>
+      <LinearProgress color="success" />
+    </>
+  );
+};
