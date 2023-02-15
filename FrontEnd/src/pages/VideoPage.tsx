f24fbcd6cc29aed6b3bcc060d6877c8f2335308a
박현우 <a40293865@gmail.com>
2023-02-16 01:13:15 +0900

    Merge1시13분

--- FrontEnd/src/pages/VideoPage.tsx
+++ FrontEnd/src/pages/VideoPage.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+//@ts-ignore
 import VideoRoomComponent from '../components/VideoRoomComponent';
 
 export function VideoPage() {
