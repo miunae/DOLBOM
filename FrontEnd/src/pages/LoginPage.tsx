c1123d5a7185579f94128fa6dcb0fd0d924a2281
박현우 <a40293865@gmail.com>
2023-02-16 15:13:23 +0900

    캘린더 에러 있음

--- FrontEnd/src/pages/LoginPage.tsx
+++ FrontEnd/src/pages/LoginPage.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 
 import { LoginForm } from '../features/auth/LoginForm';
-import { Wave } from '../features/wave/wave';
+import { Wave } from '../features/wave/Wave';
 // import { MainAppBar } from '../features/main-app-bar/MainAppBar';
 
 export const LoginPage = () => {
