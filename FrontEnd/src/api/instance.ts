53f319196757e1dbc17933f676da04264b644de9
이충무 <lcm2822@daum.net>
2023-02-16 10:59:26 +0900

    Update FrontEnd/src/api/instance.ts

--- FrontEnd/src/api/instance.ts
+++ FrontEnd/src/api/instance.ts
@@ -6,7 +6,7 @@ import { clearUser } from '../features/auth/userSlice';
 
 axios.defaults.headers.common['Content-Type'] = 'application/json';
 
-export const BASE_URL = 'https://i8c103.p.ssafy.io:5000/api';
+export const BASE_URL = 'https://i8c103.p.ssafy.io:443/api';
 
 export const axiosService = axios.create({ baseURL: BASE_URL });
 const accessToken = sessionStorage.getItem('access-token');
